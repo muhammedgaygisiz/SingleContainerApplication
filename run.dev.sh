a97e6cbdc718ce291ffd558cba8573ce7180fce2
Muhammed Gaygisiz <muhammed.gaygisiz@gmail.com>
2019-11-01 13:22:29 +0100

    Use tagged image to run dev container

--- run.dev.sh
+++ run.dev.sh
@@ -2,4 +2,4 @@
 
 # Map current folder $(pwd) to /app folder
 # but dont map /app/node_modules
-docker run -p 3000:3000 -v /app/node_modules -v "$(pwd)":/app 3bff50a0dea7
+docker run -p 3000:3000 -v /app/node_modules -v "$(pwd)":/app mgaygisiz/frontend
